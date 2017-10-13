a225db96e75e628c3a4aaf3631ee318bb595d469
mart-dominguez <mdomingu@gmail.com>
2017-10-13 12:45:06 -0300

    version demo

---
+++ src/main/java/com/mavha/cursos/java/ejemplo/jsf/BeanPaginaConversion.java
@@ -0,0 +1,61 @@
+package com.mavha.cursos.java.ejemplo.jsf;
+
+
+import com.mavha.cursos.java.ejemplo.logica.Conversor;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mdominguez
+ */
+@Named("conversorBean")
+@RequestScoped
+public class BeanPaginaConversion {
+    @Inject 
+    private Conversor conversor;
+    
+    private Double temperaturaIngreso;
+    private Double temperaturaResultado;
+    
+    public void farenheitToCelcius(){
+        this.temperaturaResultado = conversor.farenheitToCelcius(temperaturaIngreso);
+    }
+    
+    public void celciusToFarenheit(){
+        this.temperaturaResultado = conversor.celciusToFarenheit(temperaturaIngreso);
+    }
+
+    public Conversor getConversor() {
+        return conversor;
+    }
+
+    public void setConversor(Conversor conversor) {
+        this.conversor = conversor;
+    }
+
+    public Double getTemperaturaIngreso() {
+        return temperaturaIngreso;
+    }
+
+    public void setTemperaturaIngreso(Double temperaturaIngreso) {
+        this.temperaturaIngreso = temperaturaIngreso;
+    }
+
+    public Double getTemperaturaResultado() {
+        return temperaturaResultado;
+    }
+
+    public void setTemperaturaResultado(Double temperaturaResultado) {
+        this.temperaturaResultado = temperaturaResultado;
+    }
+
+    
+}
